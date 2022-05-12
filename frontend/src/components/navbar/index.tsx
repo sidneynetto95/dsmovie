db23d03fdb0a562ac3c9b5c4871c0da06d6942fe
Sidney Neto <99205146+sidneynetto95@users.noreply.github.com>
2022-05-12 00:14:59 -0300

    Alteração simples navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                     <a href="https://github.com/sidneynetto95">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/sidneynetto95</p>
                         </div>
                     </a>
                 </div>
